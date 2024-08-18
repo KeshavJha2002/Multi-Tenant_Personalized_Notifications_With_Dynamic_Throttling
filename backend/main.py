6b1fbef9f595507d189d00245a6401224e6adaa8
Keshav2002Latest <keshav2002latest@hp>
2024-08-18 11:59:58 +0530

    prepared APIs to call the producers

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from producers import producer_for_networking_topic, producer_for_comment_topic, producer_for_like_topic, producer_for_mention_topic
 
 app = FastAPI()
 
@@ -10,27 +11,34 @@ async def root():
 @app.post('/api/like_post')
 async def root(request: Request):
   body = await request.body()
+  producer_for_like_topic(body)
 
 @app.post('/api/like_comment')
 async def root(request: Request):
   body = await request.body()
+  producer_for_like_topic(body)
 
 @app.post('/api/comment_post')
 async def root(request: Request):
   body = await request.body()
+  producer_for_comment_topic(body)
 
 @app.post('/api/comment_comment')
 async def root(request: Request):
   body = await request.body()
+  producer_for_comment_topic(body)
 
 @app.post('/api/send_friend_req')
 async def root(request: Request):
   body = await request.body()
+  producer_for_networking_topic(body)
 
 @app.post('/api/send_friend_req_ack')
 async def root(request: Request):
   body = await request.body()
+  producer_for_networking_topic(body)
 
 @app.post('/api/mention')
 async def root(request: Request):
   body = await request.body()
+  producer_for_mention_topic(body)
